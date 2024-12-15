1474e5c900911e34374123a5cbeaee3b06af8d03
João Augusto Lissoni Lanjoni <joaoaugustolanjoni@gmail.com>
2024-12-15 17:48:37 -0300

    fix: update bg from hero

--- src/school_module/views/main.cljs
+++ src/school_module/views/main.cljs
@@ -8,7 +8,7 @@
   (d/div {:class "mx-auto"}
          ($ header {})
          (d/div {:className "md:container mx-auto"}
-                (d/div {:className "hero bg-base-200 min-h-screen"}
+                (d/div {:className "hero min-h-screen"}
                        (d/div {:className "hero-content flex-col lg:flex-row-reverse"}
                               (d/img {:src "https://media.istockphoto.com/id/171306436/pt/foto/vermelho-tijolo-edif%C3%ADcio-de-escola-secund%C3%A1ria-exterior.jpg?s=612x612&w=0&k=20&c=W-yKSXGKUSg0oF1Grd2E5HaqzN1WAWzjtKSE9oMAZ34="
                                       :className "max-w-sm rounded-lg shadow-2xl"})
